a58b72db4c3c863f6793cff578cf2b9c07b4c0f3
visioneagle <mwvetpatner@gmail.com>
2024-02-20 09:54:20 +0100

    edit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,11 @@
-#include <stdio.h> 
+#include <stdio.h>
+
+/**
+*main - Starting
+*return - (int) success
+*/
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-return(0);}
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
