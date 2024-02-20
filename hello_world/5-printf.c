2dfbd25ad263191faece6c2d305c6aa8a98790f5
visioneagle <mwvetpatner@gmail.com>
2024-02-20 11:23:29 +0100

    edit

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 /**
-*main 
-*Return: equal 0 success 
+*main
+*Return: equal 0 success
 */
 int main(void)
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 
 }
