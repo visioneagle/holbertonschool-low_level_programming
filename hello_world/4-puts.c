ca9fe80b30ebd0f7efa2f18b417efd2a19c7a2ab
visioneagle <mwvetpatner@gmail.com>
2024-02-20 02:12:11 +0100

    edit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,6 @@
-#include<stdio.h> 
-
-
+#include <stdio.h> 
 int main(void)
  {
-puts("Programming is like building a multilingual puzzle"/n;
-end(0)
+puts("Programming is like building a multilingual puzzle\n");
+return(0);
 }
