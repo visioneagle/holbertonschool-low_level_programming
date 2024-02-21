e5b803c43039068382b213663cd559e60e43e5ba
visioneagle <mwvetpatner@gmail.com>
2024-02-21 15:14:01 +0100

    edit

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+ * Main: déclare une variable type char puis initialise la premmère lettre de l'alphabet
+ * Return: Success
+ */
+
+int main(void)
+{
+	char alphabet = 'a';
+
+	while ( alphabet <= 'z');
+	{	
+
+		putchar (alphabet);
+
+		alphabet++;
+
+	}	
+	putchar('\n')
+		return (0);
+
+}
