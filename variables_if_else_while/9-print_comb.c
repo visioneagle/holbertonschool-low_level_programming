5daae7928d00af4acb1e456a302cacc720790fa0
visioneagle <mwvetpatner@gmail.com>
2024-02-22 17:20:40 +0100

    edit

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+*main -  all possible combinations of single-digit numbers
+*Return: valide code
+*/
+
+int main(void)
+{
+
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+
+		putchar(num);
+		if (num == 9)
+			continue;
+
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
