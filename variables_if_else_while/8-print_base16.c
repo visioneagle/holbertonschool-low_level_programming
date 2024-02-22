8dc08a121c4f048f635ab4c884f48fdf9cc7128d
visioneagle <mwvetpatner@gmail.com>
2024-02-22 14:42:11 +0100

    edit

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+*main - prints all the numbers of base 16 in lowercase
+*Return: Arrived 0 success
+*/
+int main(void)
+{
+
+	char abc123;
+
+	for (abc123 = 'a'; abc123 <= 9; abc123++)
+	{
+
+		putchar(abc123);
+
+	}
+
+	for (abc123 = '0'; abc123 <= 'f'; abc123++)
+	{
+
+		putchar(abc123)
+
+	}
+
+	putchar('\n');
+	return (0);
+
+}
