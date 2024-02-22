5cd3e3b496b979ac1d1d103381904e544197f044
visioneagle <mwvetpatner@gmail.com>
2024-02-22 15:18:55 +0100

    edit

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,14 +9,14 @@ int main(void)
 	int num;
 	char alph;
 
-	for (num = 0; num <= 10; num++)
+	for (num = 0; num < 10; num++)
 
 
 		putchar((num % 10) + '0');
 
 
 
-	for (alph = 'a'; alph <= 'f'; alph++)
+	for (alph = 'a'; alph < 'f'; alph++)
 
 
 		putchar(alph);
