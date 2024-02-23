fbf37251c9fa2d40c48c9860adb1715f43b494ba
visioneagle <mwvetpatner@gmail.com>
2024-02-23 19:54:21 +0100

    edit

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _isalpha - Checks for lowercase character
+ * @c: The character to be checked
+ *
+ * Return: 1 for lowercase character or 0 for anything else
+ */
+int _isalpha(int c)
+{
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c >= 'Z'))
+	{
+        	return (1);
+    
+	}
+
+	else
+	{
+
+	return (0);
+
+	}
+
+}
