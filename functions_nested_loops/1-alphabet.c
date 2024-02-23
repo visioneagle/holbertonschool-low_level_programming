64daf4cdcd2dd5b9cd1ff808913315b192c969d7
visioneagle <mwvetpatner@gmail.com>
2024-02-23 11:54:22 +0100

    edit

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*main - display alphabet
+*Return: ok valide 0
+*/
+int main(void)
+{
+	
+	char i;
+
+	for (i= 'a'; i <= 'z'; i++)
+
+	{
+		_putchar('i');
+
+	}
+
+        _putchar('\n');
+	return (0);
+}
