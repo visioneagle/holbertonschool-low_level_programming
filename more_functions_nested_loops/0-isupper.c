0cbe8e87e295b8ba87862afd6abf2ab310212724
visioneagle <mwvetpatner@gmail.com>
2024-02-26 15:52:58 +0100

    edit

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*_isupper - Checked upper
+*@c: Capital letter
+*Return: 1 if upper 0 always
+*/
+int _isupper(int c)
+{
+
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+
+	return (1);
+
+	}
+
+	return (0);
+
+}
