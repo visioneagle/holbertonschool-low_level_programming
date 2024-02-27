a36d2131497db8b32842230c17ba26027f0f7262
visioneagle <mwvetpatner@gmail.com>
2024-02-27 16:59:32 +0100

    edit

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_numbers - display digit
+*Return: Ok digit
+*/
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+
+	_putchar(c);
+
+	}
+	_putchar('\n');
+}
+
